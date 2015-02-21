56e3a4f631b8fbe3b2a935e9f3b2ec0a3b8e9ed1
Ryan McGregor <ryanm3685@gmail.com>
2015-02-21 16:39:46 -0500

    Location Service Impl (misspelled) additions
    getNearestLocationsAsString was added

--- src/main/java/com/citiparks/grubup/service/LocationSeriveImpl.java
+++ src/main/java/com/citiparks/grubup/service/LocationSeriveImpl.java
@@ -1,5 +1,6 @@
 package com.citiparks.grubup.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.citiparks.grubup.model.Location;
@@ -26,8 +27,8 @@ public class LocationSeriveImpl implements LocationService {
 	}
 
 	@Transactional
-	public boolean removeLocation(Integer id) {
-	    Location location = entityManager.find(Location.class, id);
+	public boolean removeLocation(String shortName) {
+	    Location location = entityManager.find(Location.class, shortName);
 	    if (null != location) {
 	      entityManager.remove(location);
 	      return true;
@@ -44,7 +45,54 @@ public class LocationSeriveImpl implements LocationService {
 
 	@Transactional
 	public Location getLocation(String shortName) {
-		return  entityManager.find(Location.class, shortName);	    
+		return entityManager.find(Location.class, shortName);	    
+	}
+
+	@Transactional
+	public List<Location> getNearestLocations(String zipcode) {
+		//CriteriaQuery<Location> c = entityManager.getCriteriaBuilder().createQuery(Location.class);
+	    //todo:  actually do a query and return that
+		Location someLocation = new Location();
+		someLocation.setAddress("123 Fake St");
+		someLocation.setCity("Pittsburgh");
+		someLocation.setState("PA");
+		someLocation.setFullName("Oiishi Bento");
+		someLocation.setLatitude(-40.0);
+		someLocation.setLongitude(-80.0);
+		someLocation.setShortName("Oiishi");
+		someLocation.setZip("15213");
+		
+		Location someLocation2 = new Location();
+		someLocation2.setAddress("999 Fifth Ave");
+		someLocation2.setCity("Pittsburgh");
+		someLocation2.setState("PA");
+		someLocation2.setFullName("Mama Rose");
+		someLocation2.setLatitude(-40.0);
+		someLocation2.setLongitude(-80.0);
+		someLocation2.setShortName("Mama");
+		someLocation2.setZip("15222");
+		
+		List<Location> l = new ArrayList();
+		l.add(someLocation);
+		l.add(someLocation2);
+		
+		return l;
+	    //return entityManager.createQuery(c).getResultList();
+
+	}
+	
+	public String getNearestLocationsAsString(String zipcode)
+	{
+		List<Location> theList = getNearestLocations(zipcode);
+		
+		String s = "";
+		for (int i = 0; i < theList.size(); i++)
+		{
+			if (i > 0) s += "\n";
+			s += theList.get(i).toString();
+		}
+		
+		return s;
 	}
 
 	
