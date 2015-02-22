1480ec4024d2d70f765a44416689de89848beb37
Tonya Edmonds <tonya.edmonds10@gmail.com>
2015-02-22 00:01:11 -0500

    Added a simple routing method for twilio requests at root directory.

--- src/main/java/com/citiparks/grubup/controller/TwilioController.java
+++ src/main/java/com/citiparks/grubup/controller/TwilioController.java
@@ -1,5 +1,7 @@
 package com.citiparks.grubup.controller;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,24 @@ import com.twilio.sdk.verbs.TwiMLResponse;
 @RequestMapping("/twilio")
 public class TwilioController {
 
+	@RequestMapping(value="/", method=RequestMethod.GET, produces={"application/xml"})
+	public String sendTwilioResponse(@RequestBody String messageReceived,  BindingResult result){
+	 	TwiMLResponse twiml = new TwiMLResponse();
+	 	Message twlMessage;
+		
+	 	if(messageReceived.equalsIgnoreCase("abc")){
+			twlMessage = new Message("received ABC");
+			try {
+				twiml.append(twlMessage);
+			} catch (TwiMLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return twiml.toXML();
+		}
+	 	return "Command not found.";
+		
+	}
 	@RequestMapping(value="/location", method=RequestMethod.GET, produces={"application/xml"})
 	public String getLocation(){
 		 	TwiMLResponse twiml = new TwiMLResponse();
